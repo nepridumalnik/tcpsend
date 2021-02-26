6a762ed797b97791e39607d52b9f0c17cc8c65d9
Дудник Павел <ne.pridumal.nik@yandex.ru>
2021-02-26 19:46:54 +0700

    GCC removed from generators

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ CONFIGURE_OPTIONS = (
 class TCPSend(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "boost/1.72.0", "pcapplusplus/20.08@bincrafters/stable"
-    generators = "cmake", "gcc", "txt"
+    generators = "cmake"
     default_options = {"boost:shared": False}
 
     def build(self):
