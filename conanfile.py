3b78e9772b19e78527a085ebca006e5fae5671f0
Дудник Павел <ne.pridumal.nik@yandex.ru>
2021-02-26 19:45:31 +0700

    conanfile.txt upgraded to conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile, CMake
+
+
+CONFIGURE_OPTIONS = (
+    "program_options"
+)
+
+
+class TCPSend(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "boost/1.72.0", "pcapplusplus/20.08@bincrafters/stable"
+    generators = "cmake", "gcc", "txt"
+    default_options = {"boost:shared": False}
+
+    def build(self):
+        build_type = self.settings.get_safe("build_type", default="Release")
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
